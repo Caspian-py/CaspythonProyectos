a930d8e942555674f707495b19bae25e89ee6274
Caspian <caspian.root@gmail.com>
2025-07-05 22:04:26 -0500

    Hemos comenzado el proyecto 3, es un gestor de contraseñas, usaremos hashlib, fernet de cryptographic y un modulo mas para copiar la contraseña al portapapeles.

---
+++ GESTOR DE CONTRASEÑAS/GestorDeContraseñas.py
@@ -0,0 +1,9 @@
+"""
+    🔹 3. Gestor de contraseñas
+    •	Funcionalidades: guardar contraseñas encriptadas, buscarlas por servicio
+    •	Módulos: hashlib o cryptography, json
+    •	Aprendes: hashing, manejo seguro de datos, persistencia de archivos.
+"""
+
+import json
+
